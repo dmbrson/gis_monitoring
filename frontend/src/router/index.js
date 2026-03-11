3f53f1a86ec8b8bbc466054ec5e9b3b83f692c65
dmbrson <dka04@list.ru>
2026-03-11 19:49:54 +1000

    Frontend ObjectCreate: new router for object create

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -22,6 +22,12 @@ const router = createRouter({
       component: () => import('@/components/ProfileView.vue'),
       meta: { requiresAuth: true },
     },
+    {
+      path: '/objects/create',
+      name: 'object-create',
+      component: () => import('@/components/ObjectCreateView.vue'),
+      meta: { requiresAuth: true, adminOnly: true },
+    },
   ],
 })
 
